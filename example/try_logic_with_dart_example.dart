245e25cd4e55f3dc2c40cd1cacb328a01b62e677
builder296 <kessirin296@gmail.com>
2021-06-16 22:36:54 +0700

    Try with 1st exam

--- example/try_logic_with_dart_example.dart
+++ example/try_logic_with_dart_example.dart
@@ -3,4 +3,25 @@ import 'package:try_logic_with_dart/try_logic_with_dart.dart';
 void main() {
   var awesome = Awesome();
   print('awesome: ${awesome.isAwesome}');
+  print('');
+
+  const y = 5;
+
+  for (var a = 1; a <= y; a++) {
+    // row
+    var row = '';
+    for (var b = 1; b <= y; b++) {
+      // column
+      if (b == 1 || b == y) {
+        row = row + 'x';
+      } else {
+        if (a == b) {
+          row = row + 'x';
+        } else {
+          row = row + 'o';
+        }
+      }
+    }
+    print(row);
+  }
 }
