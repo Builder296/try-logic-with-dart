3a7caf8f722f24bab7022c62191f6076f802c1e3
builder296 <kessirin296@gmail.com>
2021-06-16 22:36:17 +0700

    Remove 1st exam

--- example/try_logic_with_dart_example.dart
+++ example/try_logic_with_dart_example.dart
@@ -3,25 +3,4 @@ import 'package:try_logic_with_dart/try_logic_with_dart.dart';
 void main() {
   var awesome = Awesome();
   print('awesome: ${awesome.isAwesome}');
-
-
-  const y = 5;
-
-  for (var a = 1; a <= y; a++) {
-    // row
-    var row = '';
-    for (var b = 1; b <= y; b++) {
-      // column
-      if (b == 1 || b == y) {
-        row = row + 'x';
-      } else {
-        if (a == b) {
-          row = row + 'x';
-        } else {
-          row = row + 'o';
-        }
-      }
-    }
-    print(row);
-  }
 }
