84de36059af83686a323589c05b10d6ece42bbd4
builder296 <kessirin296@gmail.com>
2021-06-16 22:35:10 +0700

    first commit

---
+++ lib/try_logic_with_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library try_logic_with_dart;
+
+export 'src/try_logic_with_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ test/try_logic_with_dart_test.dart
@@ -0,0 +1,16 @@
+import 'package:try_logic_with_dart/try_logic_with_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
